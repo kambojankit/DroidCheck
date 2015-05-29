1296db7725cab934c6f92e236e52c1605ce3b456
Ankit Kamboj <er.ankitkamboj@gmail.com>
2015-05-29 19:54:03 +0530

    added properties file to handle project configurations and removed text files in favor of project wiki.

---
+++ src/main/java/com/techarha/android/security/droidcheck/analyser/DynamicAnalyser.java
@@ -0,0 +1,14 @@
+package com.techarha.android.security.droidcheck.analyser;
+
+/**
+ * Created by ankit on 28/05/15.
+ */
+public class DynamicAnalyser {
+    public static void main(String[] args) {
+
+    }
+
+    private void executeStrace(){
+
+    }
+}
